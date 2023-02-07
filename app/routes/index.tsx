408bde56c5776210e050af2955cb05d5c6fd7e3e
Alejo Boga <alejoboga20@gmail.com>
2023-02-07 09:02:36 -0500

    Styling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,15 @@
+import type { LinksFunction } from '@remix-run/node';
+import styles from '~/styles/index.css';
+
+export const links: LinksFunction = () => {
+	return [{ rel: 'stylesheet', href: styles }];
+};
+
 const Root = () => {
 	return (
-		<div>
+		<header className='header'>
 			<h1>This is the route we should see at /</h1>
-		</div>
+		</header>
 	);
 };
 
