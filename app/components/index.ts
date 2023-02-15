c9c8d6d0580802363c5f4dcd6ef8398732064807
Alejo Boga <alejoboga20@gmail.com>
2023-02-15 10:20:30 -0500

    add components and icons

---
+++ app/components/index.ts
@@ -0,0 +1,3 @@
+export { Button } from './Button/Button';
+export { InputField } from './InputField/InputField';
+export { ValidationMessage } from './ValidationMessage/ValidationMessage';
---
+++ app/routes/index.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+	return <div className='bg-gray-500 text-red-50'>HomePage</div>;
+};
+
+export default HomePage;
