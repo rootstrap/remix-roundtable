f4e4c0d915ae8792c1cff7c30eff326cd8681452
Alejo Boga <alejoboga20@gmail.com>
2023-02-15 10:10:01 -0500

    config tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,16 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import styles from '~/styles/root.css';
 
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
 	title: 'New Remix App',
 	viewport: 'width=device-width,initial-scale=1',
 });
+
+export const links: LinksFunction = () => {
+	return [{ rel: 'stylesheet', href: styles }];
+};
 
 const App = () => {
 	return (
