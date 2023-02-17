a68b6159b27f837a0f5a8dbe3ffb261a7d8b2d27
dbrosio3 <daniel.brosio@rootstrap.com>
2023-02-16 22:11:49 -0300

    cleanup

--- app/routes/pokemon-types/index.tsx
+++ app/routes/pokemon-types/index.tsx
@@ -3,12 +3,7 @@ import { Outlet } from '@remix-run/react';
 const Index = () => {
 	return (
 		<div>
-			<main>
-				{/* TODO: replace inline styles */}
-				<div style={{ display: 'flex', flexWrap: 'wrap', gap: 8 }}>
-					<Outlet />
-				</div>
-			</main>
+			<Outlet />
 		</div>
 	);
 };
